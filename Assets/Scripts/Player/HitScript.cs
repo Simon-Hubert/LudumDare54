8837aa3e46cf81009d9a632c51bd3b92541a0005
BVerstaen <bverstaen04@gmail.com>
2023-09-30 15:21:43 +0200

    Add a hitscript

---
+++ Assets/Scripts/Player/HitScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitScript : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Danger")
+        {
+            //Placer code pour crever
+        }
+    }
+}
