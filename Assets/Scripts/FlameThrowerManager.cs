ab80420a7d1619983414bad26a9c534c053395d1
BVerstaen <bverstaen04@gmail.com>
2023-09-30 22:27:12 +0200

    Add Animation / Update FlammeThrower

--- Assets/Scripts/FlameThrowerManager.cs
+++ Assets/Scripts/FlameThrowerManager.cs
@@ -1,3 +1,4 @@
+using NaughtyAttributes;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,21 +8,49 @@ public class FlameThrowerManager : MonoBehaviour
     [SerializeField] GameObject m_FlameObject;
     [SerializeField] float m_turnSpeed;
     [SerializeField] float m_flameDelay;
-    bool m_flameActive;
+    bool m_throwerActive;
+
+    [SerializeField] List<GameObject> m_flamesObjects;
+    public List<bool> m_flamesActive;
+
+    public bool throwerActive { get => m_throwerActive; set => m_throwerActive = value; }
 
     private void OnEnable() => StartCoroutine(WaitForFlame(m_flameDelay));
     private void OnDisable() => StopAllCoroutines();
 
     private void FixedUpdate()
     {
-        transform.Rotate(new Vector3(0f,0f, m_turnSpeed * Time.deltaTime));
-        m_FlameObject.SetActive(m_flameActive);
+        if (m_throwerActive)
+        {
+            transform.Rotate(new Vector3(0f, 0f, m_turnSpeed * Time.deltaTime));   
+        }
     }
+
+    public void ActivateFlames(bool active)
+    {
+        m_throwerActive = active;
+        if (m_throwerActive)
+        {
+            for (int i = 0; i < m_flamesObjects.Count; i++)
+            {
+                m_flamesObjects[i].SetActive(m_flamesActive[i]);
+            }
+        }
+        else
+        {
+            foreach(GameObject obj in m_flamesObjects) obj.SetActive(false);
+        }
+    }
+
+    [Button]
+    void DEBUG_ACTIVE() => ActivateFlames(true);
+    [Button]
+    void DEBUG_DEACTIVE() => ActivateFlames(false);
 
     IEnumerator WaitForFlame(float m_time)
     {
         yield return new WaitForSeconds(m_time);
-        m_flameActive = !m_flameActive;
+
         StartCoroutine(WaitForFlame(m_time));
     }
 }
