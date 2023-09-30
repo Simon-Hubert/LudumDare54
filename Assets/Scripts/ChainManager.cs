564bc59e64f8e8d0ed70edaba0ba95a3ca7d353f
BVerstaen <bverstaen04@gmail.com>
2023-09-30 11:24:00 +0200

    Change scene & variables names

--- Assets/Scripts/ChainManager.cs
+++ Assets/Scripts/ChainManager.cs
@@ -7,8 +7,8 @@ public class ChainManager : MonoBehaviour
     private List<Transform> m_chainSegments = new List<Transform>();
     [SerializeField] GameObject m_ChainPrefab;
     [SerializeField] private LineRenderer lineRenderer;
-    [SerializeField] private float m_chainSegLen;
-    [SerializeField] private int m_segmentLength;
+    [SerializeField] private float m_SegmentLen;
+    [SerializeField] private int m_numberOfSegments;
 
     private void Reset()
     {
@@ -22,7 +22,7 @@ public class ChainManager : MonoBehaviour
         Vector3 chainStartPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         Rigidbody2D old_rb = null;
-        for (int i = 0; i < m_segmentLength; i++)
+        for (int i = 0; i < m_numberOfSegments; i++)
         {
             GameObject newChainObject = Instantiate(m_ChainPrefab, chainStartPoint, Quaternion.identity);
             
@@ -33,7 +33,7 @@ public class ChainManager : MonoBehaviour
             old_rb = newChainObject.GetComponent<Rigidbody2D>();
 
             m_chainSegments.Add(newChainObject.transform);
-            chainStartPoint.y -= m_chainSegLen;
+            chainStartPoint.y -= m_SegmentLen;
         }
     }
 
@@ -41,7 +41,7 @@ public class ChainManager : MonoBehaviour
     {
         //Display Chain
 
-        for (int i = 0;i < m_segmentLength;i++)
+        for (int i = 0;i < m_numberOfSegments; i++)
         {
             lineRenderer.SetPosition(i, m_chainSegments[i].position);
         }
@@ -54,23 +54,23 @@ public class ChainManager : MonoBehaviour
 
         m_chainSegments[0].position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        for (int i = 0; i < m_segmentLength; i++)
+        for (int i = 0; i < m_numberOfSegments; i++)
         {
             Transform firstSegment = m_chainSegments[0];
             firstSegment.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             m_chainSegments[0] = firstSegment;
 
-            for (int j = 0; j < m_segmentLength - 1; j++)
+            for (int j = 0; j < m_numberOfSegments - 1; j++)
             {
                 Transform m_currentSegment = m_chainSegments[j];
                 Transform m_nextSegment = m_chainSegments[j + 1];
                 
                 float dist = (m_currentSegment.position - m_nextSegment.position).magnitude;
-                float error = Mathf.Abs(dist - m_chainSegLen);
+                float error = Mathf.Abs(dist - m_SegmentLen);
 
                 Vector2 changeDir = Vector2.zero;
-                if (dist > m_chainSegLen) changeDir = (m_currentSegment.position - m_nextSegment.position).normalized;
-                else if (dist < m_chainSegLen) changeDir = (m_nextSegment.position - m_currentSegment.position).normalized;
+                if (dist > m_SegmentLen) changeDir = (m_currentSegment.position - m_nextSegment.position).normalized;
+                else if (dist < m_SegmentLen) changeDir = (m_nextSegment.position - m_currentSegment.position).normalized;
 
                 Vector2 changeAmount = changeDir * error;
                 if (j != 0)
