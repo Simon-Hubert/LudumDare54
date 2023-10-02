e1847380d2e7fe7b92b569c00f0f89ccb54515b7
BVerstaen <bverstaen04@gmail.com>
2023-10-02 13:59:06 +0200

    Add life system

--- Assets/Scripts/Player/HitScript.cs
+++ Assets/Scripts/Player/HitScript.cs
@@ -5,16 +5,38 @@ using UnityEngine;
 public class HitScript : MonoBehaviour
 {
     [SerializeField] GameObject m_canvasGameOver;
+    [SerializeField] SpriteRenderer m_sR;
+    [SerializeField] int InvincibleSeconds;
+    [SerializeField] int Health;
+    bool m_canBeHit;
     bool m_isdead;
 
     public bool IsDead {  get { return m_isdead; } }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if(collision.tag == "Danger")
+        if(collision.tag == "Danger" && m_canBeHit)
         {
-            m_isdead = true;
-            m_canvasGameOver.SetActive(true);
+            m_canBeHit = false;
+            
+            Health--;
+            if (Health == 0)
+            {
+                m_isdead = true;
+                m_canvasGameOver.SetActive(true);
+            }
+            else
+            {
+                m_sR.color = new Color(1f, 1f, 1f, .5f);
+                StartCoroutine(WaitToBeHit());
+            }
         }
+    }
+
+    IEnumerator WaitToBeHit()
+    {
+        yield return new WaitForSeconds(InvincibleSeconds);
+        m_canBeHit = true;
+        m_sR.color = new Color(1f, 1f, 1f, 1f);
     }
 }
